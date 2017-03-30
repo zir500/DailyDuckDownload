c6b55784f27d3556c44d43812248ab1646d42988
zir500 <zir500@york.ac.uk>
2017-03-30 02:01:34 +0100

    Implemented getting last image from @duckoftheday
    Downloads the image attatched to the last tweet from @duck of the day
    Sets the downloaded image as the desktop background
    
    TODO:  Add twitter's CA to Ruby's openssl binding

---
+++ src/authconfig.rb
@@ -0,0 +1,9 @@
+# Unfortunatly the most convinient way of API authentication means application auth details need to be hardcoded.
+# This being a pet-project the alternatives are not very attractive:
+# Host a server to perform requests/return bearer tokens OR use user authentication with the user of the application
+
+# Fill these fields in with your application authentication details to use
+twitterAuthConfig = {
+    consumer_key: '',
+    consumer_secret: '',
+}
--- src/main.rb
+++ src/main.rb
@@ -1,15 +1,39 @@
+
 require 'rubygems'
 require 'bundler/setup'
 
 require 'win32/api'
 
+require 'twitter'
+require 'authconfig'
+
+require 'uri'
+require 'net/http'
+require 'tmpdir'
+require 'openssl'
 
 include Win32
+
 SPIF_UPDATEINIFILE = 1
 SPI_SETDESKWALLPAPER = 20
 
 SystemParametersInfo = API.new('SystemParametersInfo', 'IIPI', 'I', 'User32')
 
-pathToNewBackground = "E:\\Users\\Zain\\Pictures\\Arnold.jpg"
+# TODO: Allow twitter API's CAs
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-SystemParametersInfo.call(SPI_SETDESKWALLPAPER, 0, pathToNewBackground, SPIF_UPDATEINIFILE)
+
+twitterClient = Twitter::REST::Client.new(twitterAuthConfig)
+recentTweet = twitterClient.user_timeline('duckoftheday', count: 1, trim_user: true, exclude_replies: true, include_rts: false)
+dailyDuckURL = URI(recentTweet[0].media[0].media_url)
+
+Net::HTTP.start(dailyDuckURL.host) { |http|
+  response = http.get(dailyDuckURL.path)
+  filename = File.join(Dir.tmpdir, 'DuckofTheDay.' + dailyDuckURL.path.split('.').last)
+  file = File.open(filename, 'wb')
+  file.write(response.body)
+  SystemParametersInfo.call(SPI_SETDESKWALLPAPER, 0, filename, SPIF_UPDATEINIFILE)
+
+  file.close
+  File.delete(filename)
+}
